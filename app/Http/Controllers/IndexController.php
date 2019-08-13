089514aa93636fa356535ccf8f91462fa26008d0
Falldanger <bohdan.murga@yandex.ua>
2019-08-13 11:14:18 +0300

    display a list of articles

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class IndexController extends Controller
 {
@@ -10,6 +11,11 @@ class IndexController extends Controller
 
     	$header = 'Hello, World!';
     	$message='This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.';
-    	return view('page')->with(['message'=>$message,'header'=>$header]);
+
+    	$articles=Article::select(['id','title','desc'])->get();
+
+    	//dump($articles);
+
+    	return view('page')->with(['message'=>$message,'header'=>$header,'articles'=>$articles]);
     }
 }
